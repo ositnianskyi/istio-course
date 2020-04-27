995b9c7402387f849c6c8f00cc28acf82fe3d2d4
Oleksii Sitnianskyi <oleksii.sitnianskyi@okta.com>
2020-04-27 16:37:52 +0300

    Update session2 project files

--- session2/Project/script-publish-components.sh
+++ session2/Project/script-publish-components.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t alexpletnev/frontend-service:1.0 -f frontend/Dockerfile frontend
-#docker push alexpletnev/frontend-service:1.0
+docker build -t ositnianskyi/frontend-service:1.0 -f frontend/Dockerfile frontend
+docker push ositnianskyi/frontend-service:1.0
 
-docker build -t alexpletnev/books-service:1.0 -f books/Dockerfile books
-#docker push alexpletnev/books-service:1.0
+docker build -t ositnianskyi/books-service:1.0 -f books/Dockerfile books
+docker push ositnianskyi/books-service:1.0
 
-docker build -t alexpletnev/authors-service:1.0 -f authors/Dockerfile authors
-#docker push alexpletnev/authors-service:1.0
+docker build -t ositnianskyi/authors-service:1.0 -f authors/Dockerfile authors
+docker push ositnianskyi/authors-service:1.0
